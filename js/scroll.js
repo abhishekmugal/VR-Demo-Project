c9d89fa15e39b0a41fed0681ca0eff4cd408fe3c
Abhishek Mugal <abhishek.mugal@gmail.com>
2016-10-23 14:55:13 +0900

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -54,4 +54,43 @@
             $('html, body').animate({ scrollTop:  $("#tourism").offset().top - 110 }, 'slow');
             return false;
         });
+     
+     
+        /*for VR works page*/
+        $("a[href='#how_vr_work']").click(function(){
+            $('html, body').animate({ scrollTop:  $("#how_vr_work").offset().top - 110 }, 'slow');
+            return false;
+        });
+        $("a[href='#vr_glass']").click(function(){
+            $('html, body').animate({ scrollTop:  $("#vr_glass").offset().top - 110 }, 'slow');
+            return false;
+        });
+        $("a[href='#device']").click(function(){
+            $('html, body').animate({ scrollTop:  $("#device").offset().top - 110 }, 'slow');
+            return false;
+        });
+        $("a[href='#lead']").click(function(){
+            $('html, body').animate({ scrollTop:  $("#lead").offset().top - 110 }, 'slow');
+            return false;
+        });
+        $("a[href='#google']").click(function(){
+            $('html, body').animate({ scrollTop:  $("#google").offset().top - 90 }, 'slow');
+            return false;
+        });
+        $("a[href='#face']").click(function(){
+            $('html, body').animate({ scrollTop:  $("#face").offset().top - 90 }, 'slow');
+            return false;
+        });
+        $("a[href='#htc']").click(function(){
+            $('html, body').animate({ scrollTop:  $("#htc").offset().top - 90 }, 'slow');
+            return false;
+        });
+        $("a[href='#sam']").click(function(){
+            $('html, body').animate({ scrollTop:  $("#sam").offset().top - 90 }, 'slow');
+            return false;
+        });
+        $("a[href='#sony']").click(function(){
+            $('html, body').animate({ scrollTop:  $("#sony").offset().top - 90 }, 'slow');
+            return false;
+        });
  });
